01382497555b568532bc9783cbdb5c92b1335301
Palam Enix <marco.palma@ucr.ac.cr>
2023-05-09 01:57:37 +0000

    Actualizacion de Drupalcore

--- project/web/sites/default/settings.ddev.php
+++ project/web/sites/default/settings.ddev.php
@@ -28,7 +28,7 @@ $databases['default']['default'] = array(
   'prefix' => "",
 );
 
-$settings['hash_salt'] = 'DdOBOsZgnfdjRXaFSrYnFawoXsIXmFhdLBFLGjqYTxgacShDoBLFSoQcteoXHvIf';
+$settings['hash_salt'] = 'eaNtWRSLdHVWYnfiGwEnyvKyAhwrAPggCFOspkIRnnnzZEIkwdAmaTQctintDnkl';
 
 // This will prevent Drupal from setting read-only permissions on sites/default.
 $settings['skip_permissions_hardening'] = TRUE;
